5ff865b57ab102b411d5344422b740a1c07aedce
Stefan Lehmann <stlm@posteo.de>
2020-03-12 08:51:14 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mlutils',
+    version='0.1.0',
+    packages=['src/mlutils'],
+    url='https://github.com/stlehmann/mlutils',
+    license='MIT',
+    author='Stefan Lehmann',
+    author_email='stlm@posteo.de',
+    description='Machine learning utility package'
+)
